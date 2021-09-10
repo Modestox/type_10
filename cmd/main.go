3c9bb3053e0635379ab58148ed3d01f496d17629
SergeyKuzmitsky <90475335+SergeyKuzmitsky@users.noreply.github.com>
2021-09-11 01:11:27 +0300

    m

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,19 @@
-package app
+package main
+
 
 import (
 	"fmt"
 	"net/http"
 )
 
-func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "go start")
-}
+func main() {
 
-func Run() {
 	http.HandleFunc("/", home_page)
 	fmt.Println("Hello")
 	http.ListenAndServe(":8080", nil)
+
+}
+
+func home_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "go start")
 }
--- go.mod
+++
@@ -1,5 +0,0 @@
-module ubuntuksm.com/type10
-
-go 1.17
-
-require github.com/SergeyKuzmitsky/type_10 v0.0.0-20210910205824-53615beae10d
